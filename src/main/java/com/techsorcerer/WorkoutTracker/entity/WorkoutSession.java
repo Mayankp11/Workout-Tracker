8f7fd99d8f29853cdf547276c5dc0c1229243232
Mayank <mayankpatil2605@gmail.com>
2025-05-02 14:23:25 -0500

    Initial commit

---
+++ src/main/java/com/techsorcerer/WorkoutTracker/entity/WorkoutSession.java
@@ -0,0 +1,46 @@
+package com.techsorcerer.WorkoutTracker.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class WorkoutSession {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private LocalDate date;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+}
