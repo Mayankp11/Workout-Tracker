ae42f535ad37228f8ea8d484a2ef2427a074fb4f
Mayank <mayankpatil2605@gmail.com>
2025-05-13 20:44:45 -0500

    Removed targetArea and ExerciseName entity and repository

--- src/main/java/com/techsorcerer/WorkoutTracker/repository/TargetAreaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techsorcerer.WorkoutTracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techsorcerer.WorkoutTracker.entity.TargetAreaEntity;
-
-public interface TargetAreaRepository extends JpaRepository<TargetAreaEntity, Long> {
-
-}
